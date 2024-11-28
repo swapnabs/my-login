f5d3d24c090a51e0c60f2a6dee469129984ab289
swapnabs <swapnashimoga97@gmail.com>
2024-11-29 00:53:01 +0530

    hashrouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Signup from "./components/Signup";
 import Login from "./components/Login";
 import Home from "./components/Home";
